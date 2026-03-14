b18976c52570a72634b43999b8c44199ce2c7701
Carlos Tavares <tavarescorp@gmail.com>
2026-03-14 19:07:13 -0300

    chore: remove obsolete scripts, csv, and sql migration files prior to SaaS architecture

--- add_fds_cols.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE public.visitas 
-ADD COLUMN IF NOT EXISTS fds_qtd_skus text,
-ADD COLUMN IF NOT EXISTS fds_refrigerador text,
-ADD COLUMN IF NOT EXISTS fds_posicionamento text,
-ADD COLUMN IF NOT EXISTS fds_refrigerados text,
-ADD COLUMN IF NOT EXISTS fds_precificados text,
-ADD COLUMN IF NOT EXISTS fds_melhoria_precificacao text;
